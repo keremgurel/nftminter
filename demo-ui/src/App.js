c4e382ba645677dc0d5901122f0880e56ef08016
Kerem Gurel <keremgurell99@gmail.com>
2021-11-08 19:13:32 -0800

    playing around

--- demo-ui/src/App.js
+++ demo-ui/src/App.js
@@ -1,13 +1,21 @@
-import ReactDOM from 'react-dom';
 import { Canvas } from '@react-three/fiber';
 import './App.css';
 
 function App() {
 	return (
 		<div id='canvas-container'>
-			<Canvas />
+			<Canvas>
+				<ambientLight intensity={0.1} />
+				<directionalLight color='red' position={[0, 0, 5]} />
+				<mesh>
+					<boxGeometry />
+					<meshStandardMaterial />
+				</mesh>
+			</Canvas>
 		</div>
 	);
 }
 
 export default App;
+
+0x889Bd0Fc0a0345A413E0bcEDFeab4b2478Cc9f08
