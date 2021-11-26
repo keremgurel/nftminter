6dcd281e9b2ba178055c4198d000746febb4caa4
Kerem Gurel <keremgurell99@gmail.com>
2021-11-26 01:21:30 -0800

    change smartcontract address
    Signed-off-by: Kerem Gurel <keremgurell99@gmail.com>

--- minter/src/util/interact.js
+++ minter/src/util/interact.js
@@ -2,7 +2,7 @@ import { pinJSONToIPFS } from './pinata.js';
 require('dotenv').config();
 const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
 const contractABI = require('../contract-abi.json');
-const contractAddress = '0x889bd0fc0a0345a413e0bcedfeab4b2478cc9f08';
+const contractAddress = '0x4C4a07F737Bf57F6632B6CAB089B78f62385aCaE';
 const { createAlchemyWeb3 } = require('@alch/alchemy-web3');
 const web3 = createAlchemyWeb3(alchemyKey);
 
